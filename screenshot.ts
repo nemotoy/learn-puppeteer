bea4248028bd13e4cfe4bb213eb06bd931bf5331
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-01 20:05:43 +0900

    feat: replace with ts
    note: since install ts-node grobally, uninstall it from this project

---
+++ screenshot.ts
@@ -0,0 +1,12 @@
+import puppeteer from 'puppeteer'
+
+const main = async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('http://google.com/')
+  await page.screenshot({ path: 'example.png' })
+
+  await browser.close()
+}
+
+void main()
