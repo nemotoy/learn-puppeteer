3ab46cdaf562c6c0c154407be27a2df525c33c10
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-03 07:32:30 +0900

    fix: path to put screenshot file

--- screenshot.ts
+++ screenshot.ts
@@ -4,7 +4,7 @@ const main = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('http://google.com/')
-  await page.screenshot({ path: 'example.png' })
+  await page.screenshot({ path: './data/example.png' })
 
   await browser.close()
 }
