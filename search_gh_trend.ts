f7ecc9f099dfe17f94239e0a39fd21f065715fda
yuma-nemoto <yuma-nemoto@dac.co.jp>
2020-09-06 17:47:09 +0900

    refactor: define function that generates an url

--- search_gh_trend.ts
+++ search_gh_trend.ts
@@ -1,19 +1,22 @@
 import puppeteer from 'puppeteer'
 
+const trend_url = 'https://github.com/trending/'
+const since_param = 'since='
+
 const main = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.setViewport({ width: 1280, height: 800 })
   const lang = 'go' // TODO: enable inputting from callers
   const date_range = 'week'
-  const qs_since = 'since=' + date_range
-  const url = 'https://github.com/trending/' + lang + '?' + qs_since // TODO: implement function
+  const url = generateTrendURL(lang, date_range)
   console.log(url)
 
   await page.goto(url) // TODO: get repository URL
   await page.waitFor(3000)
   const mil = new Date().getMilliseconds().toString()
-  const file_name = 'search' + '-' + lang + '-' + date_range + '' + mil + '.png'
+  const file_name =
+    'search' + '-' + lang + '-' + date_range + '-' + mil + '.png'
   await page.screenshot({
     path: './data/gh/' + file_name,
     fullPage: true,
@@ -22,3 +25,7 @@ const main = async () => {
 }
 
 void main()
+
+const generateTrendURL = (lang: string, date_range: string) => {
+  return trend_url + lang + '?' + since_param + date_range
+}
